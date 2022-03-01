9301933ab0bad59a6c001f29097ac075447fc46f
Seonghyeon Jeong <dovb828@gmail.com>
2022-02-28 22:59:24 -0800

    deleted

--- regression/regression with R chap2.R
+++ regression/regression with R chap2.R
@@ -1,14 +1,21 @@
+#chap2.simple linear regression 
+#scatter plot
 market = read.table("c:/data/reg/market-1.txt", header=T)
 head(market)
 plot(market$X, market$Y, xlab="광고료", ylab="총판매액", pch=19)
 title("광고료와 판매액의 산점도")
+
 # attach(market)
 # plot(X, Y, xlab="광고료", ylab="총판매액", pch=19)
+
+#linear regression graph
 market.lm = lm(Y ~ X, data=market)
 summary(market.lm)
 abline(market.lm)
 identify(market$X, market$Y)
 names(market.lm) # market.lm 변수 이름 확인
+
+#residual
 resid = market.lm$residuals
 fitted = market.lm$fitted.values
 sum(resid)
@@ -16,7 +23,6 @@ sum(fitted)
 sum(market$Y)
 sum(market$X*resid)
 sum(fitted*resid)
-
 xbar=mean(market$X)
 ybar=mean(market$Y)
 xbar
@@ -26,7 +32,9 @@ text(xbar, ybar, "(8, 18.6")
 fx <- "Y-hat = -2.2696+2.61X"
 text(locator(1), fx)
 
-sum((market$Y-fitted)*(fitted-mean(market$Y)))
+#analysis of variance table
 anova(market.lm)
+
+#coefficient of determination
 313.043/(313.043+55.357)
 sqrt(6.92)
